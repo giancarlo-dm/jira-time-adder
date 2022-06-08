784800b9628b8cd8f24a0da8b88d1b9b301b1c8f
Giancarlo Dalle Mole <giancarlo.dallemole1995@gmail.com>
2022-06-08 19:43:23 -0300

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM, { Root } from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+import "./assets/fonts/roboto/font.css";
+import "./assets/styles/main.scss";
+import "./assets/styles/buttons.scss";
+import "./assets/styles/inputs.scss";
+
+const root: Root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class MyClass {
+
+    sayHello(): void {
+        console.log("Hello World", MyClass);
+    }
+}
+
+new MyClass().sayHello();
